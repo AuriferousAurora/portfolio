4cd9fae558cba5b639d8cd4a73992f21bdc92af8
Kyle Everett Lang <kyle.everett.lang@gmail.com>
2018-11-26 16:23:19 -0600

    1.2.0 Created transparent background for header on scroll.

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
+export const Header = class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      background: ''
+    }
+  }
 
-export default () => (
-  <div className='header'>
-    <div className='header__email'>kyle.everett.lang@gmail.com</div>
-  </div>
-);
+  listenScrollEvent = e => {
+    if (window.scrollY > 25) {
+      this.setState({background: 'header--transparent'})
+    } else {
+      this.setState({background: 'header--clear'})
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.listenScrollEvent)
+  }
+
+  render() {
+    return (
+      <div className={'header ' + this.state.background} >
+        <div className='header__email'>kyle.everett.lang@gmail.com</div>
+      </div>
+    );
+  }
+}
+
+export default Header;
