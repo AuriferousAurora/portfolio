035ace3d35c75a8693beadf94c195c5d4124dfd0
Kyle Everett Lang <kyle.everett.lang@gmail.com>
2019-08-06 18:09:52 -0500

    Finished first revamp of projects component.

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -2,10 +2,16 @@ import React from 'react';
 
 export default () => (
   <div className='projects'>
-    <div className='projects__title-card'>
-      <span className='title-card__text'>Projects</span>
+    <div className='projects__info-wrapper'>
+      <div className='projects__title'>
+        <span>Project Name</span>
+        
+      </div>
+      <div className='projects__description'>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis animi soluta, blanditiis asperiores earum consequatur dolore mollitia, tempora pariatur, perferendis ab possimus minus necessitatibus neque. Odit quidem iusto totam quas?
+      </div>
     </div>
-    <div className='projects__content'>
+    <div className='projects__content-wrapper'>
       <a href='https://github.com/AuriferousAurora/TDC-Fashion' target='_blank' rel='noopener noreferrer' className='project project--tdc-fashion'>
         <span className='project-title'>TDC Fashion</span>
       </a>
