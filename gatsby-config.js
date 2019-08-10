8d8643c170e97c7c372af01880fb09a8fe5aebf2
Kyle Everett Lang <kyle.everett.lang@gmail.com>
2019-08-09 20:57:40 -0500

    S3 Deploy congiguration code.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,9 @@
 module.exports = {
-  plugins: ['gatsby-plugin-sass', 'gatsby-plugin-catch-links']
+  plugins: [ 'gatsby-plugin-sass', 
+             'gatsby-plugin-catch-links',
+            {
+              resolve: 'gatsby-plugin-s3',
+              options: { bucketName: 'kyleeverettlang.com' }
+            }
+          ]
 }
