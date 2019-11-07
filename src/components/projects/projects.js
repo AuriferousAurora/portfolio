d3e0a5f35b8bb205b1a90132ada2f1e4f3712b31
Kyle Everett Lang <kyle.everett.lang@gmail.com>
2019-11-07 11:57:39 -0600

    Layout reformat for updated project section.

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -1,31 +1,64 @@
 import React from 'react';
+import { Link } from 'gatsby';
 
 export default () => (
   <div className='projects'>
-    <div className='projects__info-wrapper'>
-      <div className='projects__title'>
-        <span>Projects</span>
-        
-      </div>
-      <div className='projects__description'>
-        The majority of my work is proprietary: two of my three previous projects are inacessible to the public. The front-end React prototypes I mentioned above are included as one of the cards in this section. The login page of OSHA Card Portal-- the application that recieved the bulk of my attention --can be found <a href='https://www.oshacardportal.com' style={{'text-decoration': 'none'}} target='_blank' rel='noopener noreferrer'>here</a>. The third project I was involved with was an internal content development tool, also written in Django, that my coworker developed. My other, smaller projects, can be accessed by selecting any of the cards in this section.
-      </div>
+    <div className='projects__header'>
+      <div className='projects__title'><span>Projects</span></div>
     </div>
     <div className='projects__content-wrapper'>
-      <div className='project__spacing-element'></div>
-      <a href='https://metric-display.herokuapp.com/' target='_blank' rel='noopener noreferrer' className='project project--metric-display'>
-        <span className='project-title'>Metric Display</span>
-      </a>
-      <a href='https://github.com/AuriferousAurora/StockClub' target='_blank' rel='noopener noreferrer' className='project project--stock-club'>
-        <span className='project-title'>Stock Club</span>
-      </a>
-      <a href='https://github.com/AuriferousAurora/D8' target='_blank' rel='noopener noreferrer' className='project project--d8'>
-        <span className='project-title'>D8</span>
-      </a>
-      <a href='https://github.com/AuriferousAurora/remindPro' target='_blank' rel='noopener noreferrer' className='project project--remind-pro'>
-        <span className='project-title'>Remind Pro</span>
-      </a>
-      <div className='project__spacing-element--last'></div>
+      <div className='project-card'>
+        <div className='project-card__project-title'>
+
+        </div>
+        <div className='project-card__project-links'>
+
+        </div>
+        <div className='project-card__project-short-summary'>
+
+        </div>
+      </div>
+
+      <div className='project-card'>
+        <div className='project-card__project-title'>
+
+        </div>
+        <div className='project-card__project-links'>
+
+        </div>
+        <div className='project-card__project-short-summary'>
+          
+        </div>
+      </div>
+  
+
+      <div className='project-card'>
+          <div className='project-card__project-title'>
+
+          </div>
+          <div className='project-card__project-links'>
+
+          </div>
+          <div className='project-card__project-short-summary'>
+            
+          </div>
+      </div>
+    </div>
+    <div className='projects__footer'>
+      <Link className='link link__skills' to='/projects/'>View All Projects</Link>
     </div>
   </div>
 )
+
+  {/* <a href='https://metric-display.herokuapp.com/' target='_blank' rel='noopener noreferrer' className='project project--metric-display'>
+    <span className='project-title'>Metric Display</span>
+  </a>
+  <a href='https://github.com/AuriferousAurora/StockClub' target='_blank' rel='noopener noreferrer' className='project project--stock-club'>
+    <span className='project-title'>Stock Club</span>
+  </a>
+  <a href='https://github.com/AuriferousAurora/D8' target='_blank' rel='noopener noreferrer' className='project project--d8'>
+    <span className='project-title'>D8</span>
+  </a>
+  <a href='https://github.com/AuriferousAurora/remindPro' target='_blank' rel='noopener noreferrer' className='project project--remind-pro'>
+    <span className='project-title'>Remind Pro</span>
+  </a> */}
